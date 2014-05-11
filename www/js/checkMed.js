a5b5b3c497749279591a845fa2bd4aa71d50db76
LBJ23K <lbj23k1@gmail.com>
2014-05-11 15:59:06 +0800

    data change

--- www/js/checkMed.js
+++ www/js/checkMed.js
@@ -38,7 +38,6 @@ function check() {
             var dataLength = medData.length;
             // check
             var i = dataLength-1;
-
             for (; i >= 0 ; i--) 
             {
                 if( medData[i].year === thisYear && medData[i].month === thisMonth && medData[i].day === thisDay)
@@ -51,10 +50,10 @@ function check() {
             // 結果為真, X -> V
             if( result === true ){
                 // $('.fa.fa-times:eq(1)').attr('class','fa fa-check');
-                $('#home .medicineList').css('background-color','#66FF33');
+                $('#home .medicineList').css('background-color','#66FF66');
                 $('#home .medicineList').html('<i class="fa fa-check"></i>藥物'+
-                    '<ul>'+'<li>時間:'+medData[i].time+'</li>'+
-                    '<li>藥物'+medData[i].name+'</li>'+'</ul>');
+                    '<ul>'+'<li>時間: '+medData[i].time+'</li>'+
+                    '<li>藥物: '+medData[i].name+'</li>'+'</ul>');
                 $('#home .medicineList').click(function(){
                     $(this).children('ul').slideToggle();
                 })
@@ -63,9 +62,7 @@ function check() {
         }
         reader.readAsText(file);
     }
-
     function medFail(error) {
-        alert('medFail');
         console.log(error.code);
     }    
 
@@ -80,42 +77,43 @@ function check() {
         checkBloodsugar(file);
     }
     function checkBloodsugar(file){
-
-        var result = false; 
         var reader = new FileReader();
-        // 今天的年月日
-        var thisYear = new Date().getFullYear();
-        var thisMonth = new Date().getMonth() + 1; // 0 ~ 11
-        var thisDay = new Date().getDate();
-
+        
         reader.onloadend = function(evt) {
+
+            var result = false; 
+            
+            // 今天的年月日
+            var thisYear = new Date().getFullYear();
+            var thisMonth = new Date().getMonth() + 1; // 0 ~ 11
+            var thisDay = new Date().getDate();
+            if(thisMonth<10){
+                thisMonth = '0'+thisMonth;
+            }
+            if(thisDay<10){
+                thisDay = '0'+thisDay;
+            }
+            // alert(thisYear+' '+thisMonth+' '+thisDay);
             var temp = evt.target.result;
-            // alert(temp);
             var bloodData = JSON.parse( '['+temp.substr(1)+']' );
-
-            // alert(bloodData);
             var dataLength = bloodData.length;
-            // check
-            var i = dataLength-1;
-            // alert(i);
 
-            alert(bloodData[0].date.getFullYear());
-            for (; i >= 0 ; i--) 
+            var i = dataLength-1;
+
+            for (i; i >= 0 ; i--) 
             {
-                if( bloodData[i].date.getFullYear() === thisYear && bloodData[i].date.getMonth() === thisMonth && bloodData[i].date.getDate() === thisDay)
+                if( bloodData[i].date.substr(0,4) == thisYear && bloodData[i].date.substr(5,2) == thisMonth && bloodData[i].date.substr(8,2) == thisDay)
                 {
-                    alert('true');
                     result = true;
                     break;   
                 }
             }
             // 結果為真, X -> V
-            if( result === true ){
-                // $('.fa.fa-times:eq(0)').attr('class','fa fa-check');
+            if( result == true ){
                 $('#home .bloodsugarList').css('background-color','#66FF33');
                 $('#home .bloodsugarList').html('<i class="fa fa-check"></i>血糖'+
-                    '<ul>'+'<li>血糖值:'+bloodData[i].value+'</li>'+
-                    '<li>時間'+bloodData[i].mealtype+'</li>'+'</ul>');
+                    '<ul>'+'<li>血糖值: '+bloodData[i].value+'</li>'+
+                    '<li>時間: '+bloodData[i].mealtype+'</li>'+'</ul>');
                 $('#home .bloodsugarList').click(function(){
                     $(this).children('ul').slideToggle();
                 })
