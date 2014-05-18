0221dde540218eeb2f4ff98bc365a5dc95eb6d64
LBJ23K <lbj23k1@gmail.com>
2014-05-18 22:48:28 +0800

    hi

--- www/js/alert.js
+++ www/js/alert.js
@@ -1,9 +1,48 @@
 $(document).ready(function(){
-   $('#medRecord .submit').click(function(){
-         // alert(medTime);
-         
-         window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail); 
-        })
+    $('#medRecord .submit').click(function(){
+
+        var medName = $('#medRecord .medName :selected').text();
+        var medTime = $('#medRecord td.active').text();
+        var newDate = new Date();
+        var month = new Date().getMonth()+1;
+        var day = new Date().getDate();
+        var newDate2 = newDate.getFullYear() + '-'+(newDate.getMonth()+1)+'-'+(newDate.getDate() );
+        medData.medname = medName;
+        medData.mealtype = medTime;
+
+        if(globeData.length == 0 ){
+            data.date = newDate2;
+            data.medicine = [];
+            data.medicine.push(medData);
+            data.bloodsugar = [];
+            data.diet = [];
+            globeData.push( data );
+        }
+            else {
+                var found = false;
+                for(var i =globeData.length-1;i >= 0;i--){
+                    if(globeData[i].date == newDate2){
+                        if( globeData[i].hasOwnProperty("medicine") ) globeData[i].medicine.push(medData);
+                        else{
+                            globeData[i].medicine = [];
+                            globeData[i].medicine.push(medData);
+                        }
+                        found  = true;
+                        break;
+                    }
+                 if(!found){
+                  data.date = newDate2;
+                  data.medicine = [];
+                  data.medicine.push(medData);
+                  data.bloodsugar = [];
+                  data.diet = [];
+                  globeData.push( data );
+                }    
+              }
+               
+            }   
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail); 
+            })
    $('#alertHome .submit').click(function(){
     // alert("yo");
          var switchInstance = $(".bsNoti").data("kendoMobileSwitch");
@@ -20,7 +59,7 @@ $(document).ready(function(){
 
 function gotFS(fileSystem) 
          {
-            fileSystem.root.getFile("medicine.json", {create: true, exclusive: false}, gotFileEntry, fail);
+            fileSystem.root.getFile("data.json", {create: true, exclusive: false}, gotFileEntry, fail);
          }
 
 function gotFileEntry(fileEntry) 
@@ -30,65 +69,20 @@ function gotFileEntry(fileEntry)
 function gotFileWriter2(writer) 
 {
 
-  var medName = $('#medRecord .medName :selected').text();
-   // var medTime = $('.medTime :selected').text();
-   var medTime = $('#medRecord td.active').text();
-   var year = new Date().getFullYear();
-   var month = new Date().getMonth()+1;
-   var day = new Date().getDate();
-   console.log(medName);
- 
-   // first time --> [{json data}]
-   if( writer.length === 0 )
-   {
-      writer.onwriteend = function(){
-        $('.fa.fa-times:eq(1)').attr('class','fa fa-check');
-                $('#home .medicineList').css('background-color','#66FF33');
-                $('#home .medicineList').html(
-                    '<ul>'+'<li>時間:'+medTime+'</li>'+
-                    '<li>藥物'+medName+'</li>'+'</ul>');
-                $('#home .medicineList').click(function(){
-                    $(this).children('ul').slideToggle();
-                        // alert('w');
-                })  
-      }
-      var data = [{"name":medName,"year":year,"month":month,"day":day,"time":medTime}];
-      writer.write(data);
-   }
+    writer.onwriteend = function(){
 
-   // second time and then --> [{json data}]
-   else
-   {
-      var data = {"name":medName,"year":year,"month":month,"day":day,"time":medTime};
-      alert(data.name);
-      // write json data
-      writer.onwriteend = function(evt) {
-         writer.seek(writer.length);
-         writer.write(data);
-         // write "]"
-         writer.onwriteend = function(evt) {
-            writer.seek(writer.length);
-            writer.write("]");
-            // log "write successfully"
-            writer.onwriteend = function(evt){
-               $('.fa.fa-times:eq(1)').attr('class','fa fa-check');
-                $('#home .medicineList').css('background-color','#66FF33');
-                $('#home .medicineList').html('<i class="fa fa-check"></i>藥物'+
-                    '<ul>'+'<li>時間:'+medTime+'</li>'+
-                    '<li>藥物'+medName+'</li>'+'</ul>');
-                $('#home .medicineList').click(function(){
-                    $(this).children('ul').slideToggle();
-                        // alert('w');
-                }) 
-            }
-         };
-      };
+      $('.ok').fadeIn().delay(1500).fadeOut('slow');
+      // $('#home .medicineList').css('background-color','#66FF33');
+      //  $('#home .medicineList').html('<i class="fa fa-check"></i>藥物'+
+      //     '<ul>'+'<li>時間: '+allData[i].medicine[k].mealtype+'</li>'+
+      //     '<li>藥物: '+allData[i].medicine[k].medname+'</li>'+'</ul>');
+      // $('#home .medicineList').click(function(){
+      //     $(this).children('ul').slideToggle();
+      // })  
+check();
+    }
+    writer.write( JSON.stringify(globeData) );
 
-      // replace "]" with ","
-      writer.seek((writer.length - 1));
-      writer.write(",");
-
-   }
 }
 
 
@@ -134,7 +128,7 @@ function showAlert() {
     navigator.notification.alert(
         '該量血糖囉!',    // message
         alertDismissed,   // callback
-        'Gundam',         // title
+        'Diabetes',         // title
         '馬上量!'         // buttonName
     );
 }
--- www/js/checkMed.js
+++ www/js/checkMed.js
@@ -5,116 +5,77 @@ function check() {
       
 
     // device APIs are available
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, medGotFS, medFail);
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, bloodsugarGotFS, medFail);
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, dataGotFS, medFail);
 
-    function medGotFS(fileSystem) {
-        fileSystem.root.getFile("medicine.json", null, medGotFileEntry, medFail);
-    }
-
-    function medGotFileEntry(fileEntry) {
-        fileEntry.file(medGotFile, medFail);
-    }
-
-    function medGotFile(file) {
-        checkMedAsText(file);
-    }
-
-    // 檢查今天是否有吃過藥
-    function checkMedAsText(file) {
-        // alert('end');
-        var result = false; 
-        var reader = new FileReader();
-        // 今天的年月日
-        var thisYear = new Date().getFullYear();
-        var thisMonth = new Date().getMonth() + 1; // 0 ~ 11
-        var thisDay = new Date().getDate();
-
-        reader.onloadend = function(evt) {
-
-            var medData = JSON.parse(evt.target.result);
-            // length of medData
-
-            var dataLength = medData.length;
-            // check
-            var i = dataLength-1;
-            for (; i >= 0 ; i--) 
-            {
-                if( medData[i].year === thisYear && medData[i].month === thisMonth && medData[i].day === thisDay)
-                {
-                    result = true;
-                    break;   
-                }
-            }
-
-            // 結果為真, X -> V
-            if( result === true ){
-                // $('.fa.fa-times:eq(1)').attr('class','fa fa-check');
-                $('#home .medicineList').css('background-color','#66FF66');
-                $('#home .medicineList').html('<i class="fa fa-check"></i>藥物'+
-                    '<ul>'+'<li>時間: '+medData[i].time+'</li>'+
-                    '<li>藥物: '+medData[i].name+'</li>'+'</ul>');
-                $('#home .medicineList').click(function(){
-                    $(this).children('ul').slideToggle();
-                })
-
-            }
-        }
-        reader.readAsText(file);
-    }
+    
     function medFail(error) {
         console.log(error.code);
     }    
 
-    function bloodsugarGotFS(fileSystem) {
-        fileSystem.root.getFile("data.json", null, bloodsugarGotFileEntry, medFail);
+    function dataGotFS(fileSystem) {
+        fileSystem.root.getFile("data.json", null, dataGotFileEntry, medFail);
     }
 
-    function bloodsugarGotFileEntry(fileEntry) {
-        fileEntry.file(bloodsugarGotFile, medFail);
+    function dataGotFileEntry(fileEntry) {
+        fileEntry.file(dataGotFile, medFail);
     }
-    function bloodsugarGotFile(file) {
-        checkBloodsugar(file);
+    function dataGotFile(file) {
+        checkData(file);
     }
-    function checkBloodsugar(file){
+    function checkData(file){
         var reader = new FileReader();
-        
+        // alert('checkData');
         reader.onloadend = function(evt) {
-
+            // alert('read');
             var result = false; 
-            
-            // 今天的年月日
-            var thisYear = new Date().getFullYear();
-            var thisMonth = new Date().getMonth() + 1; // 0 ~ 11
-            var thisDay = new Date().getDate();
-            if(thisMonth<10){
-                thisMonth = '0'+thisMonth;
-            }
-            if(thisDay<10){
-                thisDay = '0'+thisDay;
-            }
-            // alert(thisYear+' '+thisMonth+' '+thisDay);
+            var result2 = false;
+            var newDate = new Date().getFullYear()+ '-'+(new Date().getMonth()+1)+'-'+new Date().getDate();
+
             var temp = evt.target.result;
-            var bloodData = JSON.parse( '['+temp.substr(1)+']' );
-            var dataLength = bloodData.length;
+            // alert(temp);
+            var allData = JSON.parse(temp);
+            // alert (allData);
+            var dataLength = allData.length;
 
             var i = dataLength-1;
-
+            // alert(dataLength);
             for (i; i >= 0 ; i--) 
             {
-                if( bloodData[i].date.substr(0,4) == thisYear && bloodData[i].date.substr(5,2) == thisMonth && bloodData[i].date.substr(8,2) == thisDay)
+                if( allData[i].date == newDate)
                 {
-                    result = true;
+                    // result = true;
                     break;   
                 }
             }
+            alert('i='+i);
+            var j = allData[i].bloodsugar.length - 1;
+            alert('j='+j);
+            if(allData[i].bloodsugar.length>0) result = true;
+
             // 結果為真, X -> V
             if( result == true ){
+                // alert('result1');
+
                 $('#home .bloodsugarList').css('background-color','#66FF33');
                 $('#home .bloodsugarList').html('<i class="fa fa-check"></i>血糖'+
-                    '<ul>'+'<li>血糖值: '+bloodData[i].value+'</li>'+
-                    '<li>時間: '+bloodData[i].mealtype+'</li>'+'</ul>');
+                    '<ul>'+'<li>血糖值: '+allData[i].bloodsugar[j].value+'</li>'+
+                    '<li>時間: '+allData[i].bloodsugar[j].mealtype+'</li>'+'</ul>');
                 $('#home .bloodsugarList').click(function(){
+                    $(this).children('ul').slideToggle();
+                })
+
+            }
+            var k = allData[i].medicine.length - 1;
+            alert('k='+k);
+            if(allData[i].medicine.length>0) result2 = true;
+            if( result2 == true ){
+                // alert('result2');
+
+                $('#home .medicineList').css('background-color','#66FF33');
+                $('#home .medicineList').html('<i class="fa fa-check"></i>藥物'+
+                    '<ul>'+'<li>時間: '+allData[i].medicine[k].mealtype+'</li>'+
+                    '<li>藥物: '+allData[i].medicine[k].medname+'</li>'+'</ul>');
+                $('#home .medicineList').click(function(){
                     $(this).children('ul').slideToggle();
                 })
 
