862570e442bc361b59dc781b7f85717cbcdd7097
Chris <justwhenithought@gmail.com>
2014-06-01 23:26:03 +0800

    fixed untracked files

--- platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
+++
@@ -1,6 +0,0 @@
-/Users/LBJ23K/Desktop/IM_Project/test4/platforms/android/ant-build/HelloCordova-debug-unaligned.apk : \
-/Users/LBJ23K/Desktop/IM_Project/test4/platforms/android/ant-build/HelloCordova.ap_ \
-/Users/LBJ23K/Desktop/IM_Project/test4/platforms/android/ant-build/classes.dex \
-/Users/LBJ23K/Desktop/IM_Project/test4/platforms/android/CordovaLib/ant-build/classes.jar \
-/Users/LBJ23K/Desktop/IM_Project/test4/platforms/android/libs/gcm.jar \
-/Users/LBJ23K/Desktop/IM_Project/test4/platforms/android/libs/android-support-v13.jar \
